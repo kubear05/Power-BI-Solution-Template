78c7f7c94812f5a35c7f7a887e161400ecbe7db2
Mohammad Ali (MOHAALI) <mohaali@microsoft.com>
2016-04-11 13:29:16 -0700

    Fixed issue with name for debug build

--- Source/Src/SalesManagement/DynamicsCRM/postdeploymentBuild.sql
+++ Source/Src/SalesManagement/DynamicsCRM/postdeploymentBuild.sql
@@ -1,21 +1,2 @@
-﻿GO
-DROP TABLE dbo.account
-GO
-DROP TABLE dbo.businessunit
-GO
-DROP TABLE dbo.lead
-GO
-DROP TABLE dbo.opportunity
-GO
-DROP TABLE dbo.opportunityproduct
-GO
-DROP TABLE dbo.product
-GO
-DROP TABLE dbo.Scribe_ReplicationStatus
-GO
-DROP TABLE dbo.systemuser
-GO
-DROP TABLE dbo.systemusermanagermap
-GO
-DROP TABLE dbo.territory
-GO
+﻿:r .\postdeployment\InsertCRMSampleData.sql
+:r .\postdeployment\InsertActualsAndTargets.sql
